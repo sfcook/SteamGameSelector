155132590017a82e6e74812d94fa8d7208266e10
Stephen <stephenfostercook@gmail.com>
2014-10-23 22:28:50 -0500

    Added test for AddAccount() with url input

--- test/Testing/Package/SteamGameSelectorTest.java
+++ test/Testing/Package/SteamGameSelectorTest.java
@@ -54,6 +54,15 @@ public class SteamGameSelectorTest {
     }
     
     @Test
+    public void testAddAccountUrl()
+    {
+        int result=selector.addAccount("https://steamcommunity.com/id/garry");
+        
+        assertTrue(result==0);
+        assertFalse(selector.getAccounts().isEmpty());
+    }
+    
+    @Test
     public void testRemoveAccount()
     {
         
