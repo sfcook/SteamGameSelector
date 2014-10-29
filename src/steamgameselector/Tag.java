03e99be029e7d0f1c2fcc793db9eb977eb82cfc2
Stephen <stephenfostercook@gmail.com>
2014-10-29 01:52:24 -0500

    Added Tag object to be used for filtering

---
+++ src/steamgameselector/Tag.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 sfcook.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package steamgameselector;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author sfcook
+ */
+public class Tag {
+    public String tag;
+    public boolean and;
+    public boolean or;
+    public boolean not;
+    
+    Tag()
+    {
+        init(null,false,false,false);
+    }
+    Tag(String tag)
+    {
+        init(tag,false,false,false);
+    }
+    Tag(String tag, boolean and, boolean or, boolean not)
+    {
+        init(tag,and,or,not);
+    }
+    
+    private void init(String tag, boolean and, boolean or, boolean not)
+    {
+        this.tag=tag;
+        this.and=and;
+        this.or=or;
+        this.not=not;
+    }
+    
+    
+    //booleans are not involved in compares
+    @Override
+    public boolean equals(Object other){
+        return other!=null && other instanceof Tag &&
+                tag.equals(((Tag)other).tag);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.tag);
+        return hash;
+    }
+}
